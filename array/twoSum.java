0c6f2bd710a4ebab9a7ee55e2e77954183e4bb76
kajal1sharma <kajal1231995@gmail.com>
2021-03-20 16:58:55 +0530

    20/03

--- array/twoSum.java
+++ array/twoSum.java
@@ -1,30 +1,49 @@
 package array;
 import java.util.*;
 public class twoSum{
-
+//with repetative elements try hashing
     public static void main(String[] args) {
         //two elements can make up for this sum 
-        int arr[]={2,4,7,12,7,5,8};
+        int arr[]={2,4,7,12,17,5,8};
         int key=9;
 
-//sorting
-        Arrays.sort(arr);
-        int i=0;
-        int j=arr.length-1;
+        //hashing
 
-        while(i<j){
-            int sum=arr[i]+arr[j];
-            if(sum==key){
-                System.out.print(arr[i]+" "+arr[j]);
+        int hash[]=new int[50];
+        for(int i=0;i<arr.length;i++){
+            hash[arr[i]]=1;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            int sum=arr[i];
+            int left=key-sum;
+            if(hash[left]==1){
+                System.out.println(arr[i]+" "+left);
                 break;
             }
-            else if(sum>key){
-                j--;
-            }
-            else if(sum<key){
-                i++;
-            }
         }
+
+
+
+
+//sorting
+        // Arrays.sort(arr);
+        // int i=0;
+        // int j=arr.length-1;
+
+        // while(i<j){
+        //     int sum=arr[i]+arr[j];
+        //     if(sum==key){
+        //         System.out.print(arr[i]+" "+arr[j]);
+        //         break;
+        //     }
+        //     else if(sum>key){
+        //         j--;
+        //     }
+        //     else if(sum<key){
+        //         i++;
+        //     }
+        // }
 
 
 
