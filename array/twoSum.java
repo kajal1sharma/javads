f11d49aa91b1daec49c0dac95a3d4885b78a4146
kajal1sharma <kajal1231995@gmail.com>
2021-03-20 16:26:45 +0530

    20/3

---
+++ array/twoSum.java
@@ -0,0 +1,9 @@
+public class twoSum{
+
+    public static void main(String[] args) {
+        //two elements can make up for this sum 
+        int arr[]={2,4,7,12,7,5,8};
+        int key=9;
+
+    }
+} 
