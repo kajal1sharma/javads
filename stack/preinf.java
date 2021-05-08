4457582fafd1a2a500dbde5feb8ac9a679983b45
kajal1sharma <kajal1231995@gmail.com>
2021-05-08 16:42:15 +0530

    8/5

---
+++ stack/preinf.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class preinf {
+
+    public static void  toinfix(String s){
+        Stack<String> st =new Stack<String>();
+
+    }
+    public static void main(String[] args) {
+        String s="*/CD+AB";
+        toinfix(s);
+    }
+}
