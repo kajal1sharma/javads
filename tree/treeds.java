f462433d5e9e1d46dd6e98a32d6f96d5c4c1b8f7
kajal1sharma <kajal1231995@gmail.com>
2021-05-16 17:35:01 +0530

    16/5

--- tree/treeds.java
+++ tree/treeds.java
@@ -13,29 +13,43 @@ class Node{
     }
 } 
 public class treeds {
-    public static void inordernonrecursive(Node root){
-        Stack<Node> st=new Stack<Node>();
+    // public static void inordernonrecursive(Node root){
+    //     Stack<Node> st=new Stack<Node>();
     
-        while(true){
-            //inserting the left part
-            while(root!=null){
-                st.push(root);
-                root=root.left;
-            }
-            if(st.empty()){
-                break;
-            }
-            root=st.peek();
-            st.pop();
+    //     while(true){
+    //         //inserting the left part
+    //         while(root!=null){
+    //             st.push(root);
+    //             root=root.left;
+    //         }
+    //         if(st.empty()){
+    //             break;
+    //         }
+    //         root=st.peek();
+    //         st.pop();
             
-            System.out.print(root.data+"  ");
+    //         System.out.print(root.data+"  ");
             
-            root=root.right;
+    //         root=root.right;
             
             
-        }
+    //     }
         
+    //     }
+    public static Node  insertbst(Node root,int data){
+
+        if(root==null){
+            Node temp =new Node(data);
+            return temp;
         }
+        if(data<root.data){
+            root.left=insertbst(root.left, data);
+        }
+        else{
+            root.right=insertbst(root.right, data);
+        }
+        return root;
+    }
     public static void inorder(Node root){
         if(root==null){
             return ;
@@ -47,21 +61,21 @@ public class treeds {
    public static void main(String[] args) {
        Node root=null;
 
-       Node n1=new Node(12);
+       Node n1=new Node(20);
        root=n1;
 
-       n1=new Node(14);
-       root.left=n1;
        n1=new Node(16);
+       root.left=n1;
+       n1=new Node(21);
        root.right=n1;
     
-       root.left.left=new Node(18);
-       root.left.right=new Node(20);
+       root.left.left=new Node(5);
+       root.left.right=new Node(18);
 
-       root.right.left=new Node(22);
-       root.right.right=new Node(24);
-
+       //root.right.left=new Node(22);
+       root.right.right=new Node(22);
+        insertbst(root,19);
        inorder(root);
-       inordernonrecursive(root);
+      // inordernonrecursive(root);
    } 
 }
